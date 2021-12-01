cc730577a571900723d817a0316c4cfebcc093b2
Adobski <andrew@ameltek.co.uk>
2021-12-01 21:24:35 +0000

    Ability to set robot on the grid Test

--- RobotsFromMars/Robot.cs
+++ RobotsFromMars/Robot.cs
@@ -2,9 +2,20 @@
 {
     public class Robot
     {
-        public Robot()
-        {
+        private int _yAxis;
+        private int _xAxis;
+        private string _orientation;
 
+        public void SetGridPosition(int x, int y, string orientation)
+        {
+            _xAxis = x;
+            _yAxis = y;
+            _orientation = orientation;
+        }
+
+        public string Output()
+        {
+            return $"{_xAxis} {_yAxis} {_orientation}";
         }
     }
 }
---
+++ RobotsFromMarsTests/RobotTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using RobotsFromMars;
+
+namespace RobotsFromMarsTests
+{
+    [TestFixture]
+    public  class RobotTests
+    {
+        [Test]
+        public void Set_start_position_for_robot_on_grid_Test()
+        {
+            var robot = new Robot();
+
+            robot.SetGridPosition(0, 0, "N");
+
+            robot.Output()
+                .Should()
+                .Be("0 0 N");
+        }
+    }
+}
